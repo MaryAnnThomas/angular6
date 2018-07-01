87ec8b9ad9248af669615bcb358141dec972afbe
xavier-experion <xavier.ki@experionglobal.com>
2018-07-01 23:37:47 +0530

    modified data servcie for id fix

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Observable } from 'rxjs';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -11,14 +11,14 @@ import { ActivatedRoute } from "@angular/router";
 export class DetailsComponent implements OnInit {
 
   user$: Object;
-  
-  constructor(private route: ActivatedRoute, private data: DataService) { 
-     this.route.params.subscribe( params => this.user$ = params.id );
+
+  constructor(private route: ActivatedRoute, private data: DataService) {
+    this.route.params.subscribe(params => this.user$ = params.id);
   }
 
   ngOnInit() {
     this.data.getUser(this.user$).subscribe(
-      data => this.user$ = data 
+      data => this.user$ = data
     );
   }
 
